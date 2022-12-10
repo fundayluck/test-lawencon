1e038b3d5cc785b5a0788028a2db09b1ca2ddfdc
Afan Dayu Laksono <48554914+fundayluck@users.noreply.github.com>
2022-12-11 01:15:27 +0700

    adding filter box

--- src/page/ListMoviePage.js
+++ src/page/ListMoviePage.js
@@ -5,15 +5,13 @@ import Loader from '../components/Loader'
 
 
 const AppPage = () => {
-    const [title, setTitle] = useState('Batman')
+    const [title, setTitle] = useState('')
     const [movies, setMovies] = useState([])
     const [page, setPage] = useState(1)
     const [loading, setLoading] = useState(false)
     useEffect(() => {
         const fetch = async () => {
-
-            const response = await axios.get(`https://www.omdbapi.com?apikey=715289b&s=${title}&page=${page}`)
-
+            const response = await axios.get(`https://www.omdbapi.com?apikey=715289b&s=batman&page=${page}`)
             if (response.data.Response === "True") {
                 setMovies((prev) => {
                     return [...prev, ...response.data.Search]
@@ -43,12 +41,25 @@ const AppPage = () => {
         }
     };
 
-
+    const filteredMovies = movies.filter
+        ? movies.filter(
+            (item) =>
+            (item.Title &&
+                item.Title.toLowerCase().includes(title.toLowerCase()))
+        )
+        : [];
+    console.log(filteredMovies);
 
     return (
         <div className='flex flex-col items-center px-10 bg-[#222222]'>
             <h1 className='font-bold text-[50px] mb-2 text-[#F3EFE0] underline'>Movie Collection</h1>
-            <Card movies={movies} />
+            <input
+                className='p-2 mb-2 border-2'
+                placeholder='Search by title'
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+            />
+            <Card movies={filteredMovies} />
             {loading && <Loader />}
         </div>
     )
