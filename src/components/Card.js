5905f67ddd673d3c6d2158c7bb328a06a4501617
Afan Dayu Laksono <48554914+fundayluck@users.noreply.github.com>
2022-12-10 15:37:37 +0700

    Use Infinite Scroll

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,66 +1,30 @@
-import { useEffect, useState } from "react"
-import axios from "axios"
-
-
-const Card = ({ title }) => {
-    const [movies, setMovies] = useState([])
+const Card = ({ movies }) => {
+    let content;
     console.log(movies);
-    const [page, setPage] = useState(1)
-    console.log("movie", movies);
-    useEffect(() => {
-        const fetch = async () => {
-            const response = await axios.get(`https://www.omdbapi.com?apikey=715289b&s=${title}&page=${page}`)
-            if (response.data.Response === 'True') {
-                setMovies((prev) => [...prev, ...response.data.Search])
-            } else if (response.data.Response === "False") {
-                setMovies(response.data.Error)
-            }
-        }
-        fetch()
-    }, [page, title])
-
-
-    useEffect(() => {
-        window.addEventListener("scroll", handleScroll);
-
-        return () => window.removeEventListener("scroll", handleScroll);
-    }, []);
-
-    const handleScroll = async () => {
-        if (
-            window.innerHeight + document.documentElement.scrollTop + 1 >=
-            document.documentElement.scrollHeight
-        ) {
-            setPage((prev) => prev + 1);
-        }
-    };
-    return (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+    if (movies.Response === "False") {
+        content = <div className="flex justify-center">{movies.Error}</div>
+    } else {
+        content = <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
             {movies.map((movie, index) =>
-                <div className="flex justify-center" key={index}>
-                    <div className="rounded-lg shadow-lg bg-white max-w-sm" >
+                <div className="flex justify-center " key={index}>
+                    <div className="rounded-lg shadow-lg bg-white max-w-sm bg-[#434242]" >
                         <img className="rounded-t-lg md:w-[350px] md:h-[300px] lg:w-[350px] lg:h-[400px]" src={movie.Poster} alt="" />
                         <div className="p-6">
-                            <h5 className="text-gray-900 text-md font-medium mb-2">
+                            <h5 className="text-[#F3EFE0] text-md font-medium mb-2">
                                 {movie.Title}
                             </h5>
-                            <p className="text-gray-700  mb-4">
+                            <p className="text-[#F3EFE0] mb-4">
                                 {movie.Year}
                             </p>
-                            {/* <a
-                                // href={item.link}
-                                target="_blank"
-                                rel="noreferrer"
-                                className="inline-block px-6 py-2.5 bg-[#9587E4] text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-300 hover:shadow-lg  focus:shadow-lg focus:outline-none focus:ring-0 transition duration-150 ease-in-out">
-                                Detail
-                            </a> */}
+
                         </div>
                     </div>
                 </div>
             )}
         </div >
+    }
 
-    )
+    return <div>{content}</div>
 }
 
 export default Card
--- src/page/ListMoviePage.js
+++ src/page/ListMoviePage.js
@@ -1,18 +1,48 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 import Card from '../components/Card'
 
 const AppPage = () => {
-    const [title, settitle] = useState('batman')
+    const [title, setTitle] = useState('dragon')
+    const [movies, setMovies] = useState([])
+    const [page, setPage] = useState(1)
+    useEffect(() => {
+        const fetch = async () => {
+            const response = await axios.get(`https://www.omdbapi.com?apikey=715289b&s=${title}&page=${page}`)
+            if (response.data.Response === "True") {
+                setMovies(prev => [...prev, ...response.data.Search])
+            } else if (response.data.Response === "False") {
+                setMovies(response.data)
+            }
+        }
+        fetch()
+    }, [page, title])
 
+
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
+    const handleScroll = async () => {
+        if (
+            window.innerHeight + document.documentElement.scrollTop + 1 >=
+            document.documentElement.scrollHeight
+        ) {
+            setPage((prev) => prev + 1);
+        }
+    };
     return (
-        <div className='flex flex-col items-center p-5'>
-            <input
+        <div className='flex flex-col items-center px-5 bg-[#222222]'>
+            <h1 className='font-bold text-[50px] mb-2 text-[#F3EFE0]'>Movie Collection</h1>
+            {/* <input
                 className='p-2 mb-2 border-2'
                 placeholder='Search by title'
                 value={title}
-                onChange={(e) => settitle(e.target.value)}
-            />
-            <Card title={title} />
+                onChange={(e) => setTitle(e.target.value)}
+            /> */}
+            <Card movies={movies} />
         </div>
     )
 }
